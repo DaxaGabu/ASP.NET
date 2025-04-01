a15e2f61d6681494aaa51d6d83165540c9ea0e39
DaxaGabu <dgabu151@rku.ac.in>
2025-04-01 11:06:51 +0530

    session

---
+++ .NET - P/LoginLogout/LoginLogout/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginLogout.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
